8e4c31e900b5620226021dd634de57512b28c066
Lucas Holmquist <lholmqui@redhat.com>
2019-03-25 16:51:13 -0400

    feat: add kube-probe
    also removes the development flag

--- app.js
+++ app.js
@@ -3,21 +3,24 @@
 // const opossum = require('opossum');
 const express = require('express');
 const bodyParser = require('body-parser');
- 
+const probe = require('kube-probe');
+
 const app = express();
 const port = process.argv[2] || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
- 
+
 app.use('/api/greeting', (request, response) => {
   // console.log(opossum.metrics);
   // const name = request.query.name ? request.query.name : 'World';
   // response.send({content: `Hello, ${name}!`});
   response.send('foo');
 });
- 
+
 // app.get('/metrics', (request, response) => {
 //   response.send(opossum.metrics.metrics());
 // });
- 
+
+probe(app);
+
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
