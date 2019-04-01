7482f242a8072fbfb5f25aee1530a933057e0a3c
Helio Frota <00hf11@gmail.com>
2019-04-01 10:05:20 -0300

    src: removing unnecessary settings when using fedora

--- minishift-start.sh
+++ minishift-start.sh
@@ -5,7 +5,3 @@ minishift config set cpus 2
 minishift config set memory 4096
 
 minishift start
-# minishift start --show-libmachine-logs -v5
-
-minishift addon install --defaults
-minishift addon apply registry-route
