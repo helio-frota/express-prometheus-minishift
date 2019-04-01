c194154052acc02d3d9726f311f031b460feea08
Helio Frota <00hf11@gmail.com>
2019-04-01 10:16:57 -0300

    src: better project structure

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -5,4 +5,4 @@ npm install
 echo 'minishift login'
 oc login -u developer
 echo 'nodeshift deploy'
-npx nodeshift --strictSSL=false --expose --imageTag="10.15.1" --build.recreate=buildConfig
+npx nodeshift --strictSSL=false --expose --imageTag="10.15.3" --build.recreate=buildConfig
